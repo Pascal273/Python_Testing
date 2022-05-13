aa3f5091e8c1dd3674472aa043c1875f121043e0
Pascal <77117642+Pascal273@users.noreply.github.com>
2022-05-13 19:32:32 +0200

    tests added

---
+++ tests/unit/test_functions.py
@@ -0,0 +1,28 @@
+from server import app, competitions, clubs
+
+
+def test_purchasePlaces():
+    """
+    GIVEN a secretary who wants to book places in a competition
+    WHEN he tries to book x number of places
+    THEN he should not be able to use more than their points allowed
+    """
+    with app.test_client() as test_client:
+        club = clubs[1]
+        competition = competitions[0]
+        url = '/purchasePlaces'
+        enough_points_response = test_client.post(url, data={
+            'club': club['name'],
+            'competition': competition['name'],
+            'places': '4',
+        })
+        assert b'Great-booking complete!' in enough_points_response.data
+        assert enough_points_response.status_code == 200
+
+        not_enough_points_response = test_client.post(url, data={
+            'club': club['name'],
+            'competition': competition['name'],
+            'places': '6',
+        })
+        assert b'Great-booking complete!' not in not_enough_points_response.data
+        assert not_enough_points_response.status_code == 200
