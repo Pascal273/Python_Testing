d13a2534223bf5a29111520f2476bf496c8f8124
Pascal <77117642+Pascal273@users.noreply.github.com>
2022-05-16 19:05:27 +0200

    test for book-endpoint added

--- tests/unit/test_functions.py
+++ tests/unit/test_functions.py
@@ -1,7 +1,7 @@
-from server import app, competitions, clubs
+from server import app, competitions, clubs, url_for
 
 
-def test_purchasePlaces():
+def test_book():
     """
     GIVEN a secretary wishes to book a number of places for a competition
     WHEN he tries to book a number of places on a competition that has
@@ -9,4 +9,10 @@ def test_purchasePlaces():
     THEN he  should not be able to book a place on a post-dated competition
     """
     with app.test_client() as test_client:
-        pass
+        club = clubs[0]
+        competition = competitions[0]
+        url = f'book/{competition["name"]}/{club["name"]}'
+        invalid_comp_response = test_client.get(url)
+        print(invalid_comp_response.data)
+        assert invalid_comp_response.status_code == 200
+        assert b'Sorry, but you cant book places on past competitions.' in invalid_comp_response.data
