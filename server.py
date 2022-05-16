322ed53bb6e4df83a0a2333eaca613412bb70077
Pascal <77117642+Pascal273@users.noreply.github.com>
2022-05-16 18:32:01 +0200

    Bug fix moved from purchasePlaces to book endpoint to display a message instead of rendering the booking page for past competitions.

--- server.py
+++ server.py
@@ -36,36 +36,34 @@ def showSummary():
 def book(competition, club):
     foundClub = [c for c in clubs if c['name'] == club][0]
     foundCompetition = [c for c in competitions if c['name'] == competition][0]
-    if foundClub and foundCompetition:
-        return render_template(
-            'booking.html', club=foundClub, competition=foundCompetition)
-    else:
-        flash("Something went wrong-please try again")
-        return render_template(
-            'welcome.html', club=club, competitions=competitions)
+
+    # Bug #4 fixed - places in past competitions can't be booked,
+    # error-message will be displayed instead of the booking page.
+    competition_date_obj = datetime.strptime(
+        foundCompetition["date"], '%Y-%m-%d %H:%M:%S').date()
+    today_date_obj = datetime.today().date()
+
+    if today_date_obj <= competition_date_obj:
+        if foundClub and foundCompetition:
+            return render_template(
+                'booking.html', club=foundClub, competition=foundCompetition)
+        else:
+            flash("Something went wrong-please try again")
+            return render_template(
+                'welcome.html', club=foundClub, competitions=competitions)
+    flash('Sorry, but you cant book places on past competitions.')
+    return render_template(
+        'welcome.html', club=foundClub, competitions=competitions)
 
 
 @app.route('/purchasePlaces', methods=['POST'])
 def purchasePlaces():
     competition = [c for c in competitions if c['name'] == request.form['competition']][0]
     club = [c for c in clubs if c['name'] == request.form['club']][0]
-
-    # Bug #4 fixed - places in past competitions can't be booked,
-    # error-message will be displayed instead of confirmation message.
-    competition_date_obj = datetime.strptime(
-        competition["date"], '%Y-%m-%d %H:%M:%S').date()
-    today_date_obj = datetime.today().date()
-
-    if today_date_obj <= competition_date_obj:
-        placesRequired = int(request.form['places'])
-        competition['numberOfPlaces'] = int(
-            competition['numberOfPlaces']) - placesRequired
-        flash('Great-booking complete!')
-        return render_template(
-            'welcome.html', club=club, competitions=competitions)
-    flash('Sorry, but you cant book places on past competitions.')
-    return render_template(
-        'welcome.html', club=club, competitions=competitions)
+    placesRequired = int(request.form['places'])
+    competition['numberOfPlaces'] = int(competition['numberOfPlaces']) - placesRequired
+    flash('Great-booking complete!')
+    return render_template('welcome.html', club=club, competitions=competitions)
 
 
 # TODO: Add route for points display
