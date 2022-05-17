c06f51768b234e41160c0e2cd9bd35150fa6bbc4
Pascal <77117642+Pascal273@users.noreply.github.com>
2022-05-17 20:38:15 +0200

    test for purchesPlaces added to ensure the points of the club are getting updated.

---
+++ tests/unit/test_functions.py
@@ -0,0 +1,27 @@
+from server import app, competitions, clubs
+from pprint import pprint
+
+
+def test_purchasePlaces():
+    """
+    GIVEN a secretary wishes to redeem points for a place in a competition
+    WHEN The number of places is confirmed.
+    THEN The amount of points used should be deducted from the club's balance.
+    """
+    with app.test_client() as test_client:
+        club = clubs[0]
+        competition = competitions[0]
+        url = '/purchasePlaces'
+
+        points = int(club["points"])
+        places = 1
+
+        places_booked_response = test_client.post(url, data={
+            'club': club['name'],
+            'competition': competition['name'],
+            'places': places,
+        })
+
+        correct_result = f'Points available: {points-places}'
+        assert bytes(correct_result, 'utf-8') in places_booked_response.data
+        assert places_booked_response.status_code == 200
